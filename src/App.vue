618ede7811e8b6ab045603358d1b59d3677d6f5a
bbuccianti <bebuccianti@gmail.com>
2018-02-09 20:51:11 -0300

    add header and semantic ui

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <Header></Header>
     <router-view/>
   </div>
 </template>
 
 <script>
+import Header from './components/Header';
+
 export default {
   name: 'App',
+  components: {
+    Header,
+  },
 };
 </script>
 
