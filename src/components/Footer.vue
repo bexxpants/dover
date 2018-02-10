c210a3cd6263baf14d79615536cbb2ea2af750b8
bbuccianti <bebuccianti@gmail.com>
2018-02-10 16:26:58 -0300

    added footer component to app

---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer class="ui inverted vertical footer segment">
+
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+};
+</script>
+
+<style>
+
+</style>
