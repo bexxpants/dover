ac09d502358d869d0840afcde3d14ff878a64eec
bbuccianti <bebuccianti@gmail.com>
2018-02-09 21:36:10 -0300

    created home component

---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div id="logo">Logo</div>
+    <button class="ui primary button">Create a project</button>
+    <button class="ui primary button">Work as a freelancer</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+};
+</script>
+
+<style scoped>
+#logo {
+  width: 150px;
+  height: 150px;
+  margin: 0 auto;
+  margin-bottom: 50px;
+  border: 5px solid black;
+}
+</style>
