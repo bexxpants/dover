e1ec33c927ec0d56beda95b8f521f353fca3cdc1
bbuccianti <bebuccianti@gmail.com>
2018-02-10 19:48:26 -0300

    sign up component created

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
   <div class="ui secondary menu">
     <router-link to="/" class="header item">dover</router-link>
     <div class="right menu">
-      <router-link to="#" class="ui button primary">Sign up</router-link>
+      <router-link to="signup" class="ui button primary">Sign up</router-link>
       <router-link to="login" tag ="button" class="ui button">Login</router-link>
     </div>
   </div>
---
+++ src/components/SignUp.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="ui center aligned grid">
+    <div class="eight wide column">
+      <div class="ui header">Sign up</div>
+      <div class="ui large form">
+        <div class="ui raised segment">
+          <div class="field">
+            <div class="ui left icon input">
+              <i class="user icon"></i>
+              <input type="text" name="email" placeholder="E-mail address" >
+            </div>
+          </div>
+          <div class="field">
+            <div class="ui left icon input">
+              <i class="lock icon"></i>
+              <input type="password" name="password" placeholder="Password">
+            </div>
+          </div>
+          <div class="field">
+            <button class="ui button">Sign Up</button>
+          </div>
+        </div>
+        <div class="ui header">Or sign up with:</div>
+        <div class="ui raised segment">
+          <div class="field">
+            <a class="ui github button">
+              <i class="github icon"></i>
+              GitHub
+            </a>
+            <a class="ui google plus button">
+              <i class="google plus icon"></i>
+              Google
+            </a>
+            <a class="ui facebook button">
+              <i class="facebook icon"></i>
+              Facebook
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SignUp',
+};
+</script>
+
+<style>
+.ui.center.aligned.grid {
+  margin-top: 5em;
+  margin-bottom: 5em;
+}
+</style>
