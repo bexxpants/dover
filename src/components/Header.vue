69eb67deb7650f5761e5bffcfdfc73ce7b72c8f2
bbuccianti <bebuccianti@gmail.com>
2018-02-09 21:07:38 -0300

    simple header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,11 @@
 <template>
-
+  <div class="ui menu">
+    <a href="#" class="header item">dover</a>
+    <div class="right menu">
+      <a href="#" class="ui item">Sign up</a>
+      <a href="#" class="ui item">Login</a>
+    </div>
+  </div>
 </template>
 
 <script>
