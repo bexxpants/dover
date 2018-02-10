1f44026247b32b36a7636b50a1ea741c8ac2712f
bbuccianti <bebuccianti@gmail.com>
2018-02-10 15:42:00 -0300

    added login component

---
+++ src/components/Login.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="ui center aligned grid">
+    <div class="column">
+      <div class="ui header">Login to your account</div>
+      <div class="ui large form">
+        <div class="ui stacked segment">
+          <div class="field">
+            <div class="ui left icon input">
+              <i class="user icon"></i>
+              <input type="text" name="email" placeholder="E-mail address">
+            </div>
+          </div>
+          <div class="field">
+            <div class="ui left icon input">
+              <i class="lock icon"></i>
+              <input type="passwork" name="password" placeholder="Password">
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="ui message">
+        New user?
+        <router-link to="/" tag="button" class="ui button primary">Sign up</router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+};
+</script>
