feeed78f9c5dd0e3149de8225c336a440d34c173
bbuccianti <bebuccianti@gmail.com>
2018-02-13 19:51:20 -0300

    added vue-axios and vuex

--- src/main.js
+++ src/main.js
@@ -2,16 +2,21 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import SuiVue from 'semantic-ui-vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 import App from './App';
 import router from './router';
+import store from './store';
 
 Vue.config.productionTip = false;
 Vue.use(SuiVue);
+Vue.use(VueAxios, axios);
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>',
 });
