057b5e4d9dc7f3a00802a88a533a3cad2d813e3e
bbuccianti <bebuccianti@gmail.com>
2018-02-16 22:12:31 -0300

    add modal to confirm the deletion of a project

--- src/components/Dashboard/projectsCreated.vue
+++ src/components/Dashboard/projectsCreated.vue
@@ -1,14 +1,14 @@
 <template>
-<sui-card-group :items-per-row="2">
+<sui-card-group :items-per-row="1">
   <sui-card v-for="project in projects" :key="project._id">
     <sui-card-content>
       <sui-card-header>
         {{project.name}}
         <sui-dropdown icon="options" id="optionsButton" :options="[]">
           <sui-dropdown-menu>
-            <button class="ui button fluid" @click="deleteProject(project._id)">
-              <sui-icon name="trash" /> Delete
-            </button>
+            <sui-button fluid @click.native="toggle(project._id)" icon="trash">
+              Delete
+            </sui-button>
           </sui-dropdown-menu>
         </sui-dropdown>
       </sui-card-header>
@@ -17,12 +17,33 @@
       <sui-card-description>{{project.description}}</sui-card-description>
     </sui-card-content>
   </sui-card>
+  <sui-modal v-model="open" size="tiny" closable>
+    <sui-modal-header>Are you sure?
+      <sui-button floated="right" @click.native="toggle('')" icon="remove" />
+    </sui-modal-header>
+    <sui-modal-content >
+      <sui-modal-description>
+        <p>The deletion can't be undone. Continue?</p>
+      </sui-modal-description>
+    </sui-modal-content>
+    <sui-modal-actions>
+      <sui-button floated="right" @click.native="deleteProject(id)">
+        Delete
+      </sui-button>
+    </sui-modal-actions>
+  </sui-modal>
 </sui-card-group>
 </template>
 
 <script>
 export default {
   name: 'projectsCreated',
+  data() {
+    return {
+      open: false,
+      id: '',
+    };
+  },
   props: {
     projects: {
       type: Array,
@@ -30,10 +51,14 @@ export default {
     },
   },
   methods: {
-    deleteProject(data) {
-      this.axios.post('projects/delete', {
-        id: data,
-      }).then(() => this.$emit('projectDeleted'));
+    toggle(id) {
+      this.open = !this.open;
+      this.id = id;
+    },
+    deleteProject(id) {
+      this.axios.post('projects/delete', { id })
+        .then(() => this.$emit('projectDeleted'))
+        .then(() => this.toggle(''));
     },
   },
 };
