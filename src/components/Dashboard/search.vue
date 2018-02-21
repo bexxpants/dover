72d666eb267891a773540a779783d47dec896cc1
bbuccianti <bebuccianti@gmail.com>
2018-02-21 19:07:02 -0300

    added simple search for name and description of projects

--- src/components/Dashboard/search.vue
+++ src/components/Dashboard/search.vue
@@ -1,9 +1,51 @@
 <template>
-  <h1>search</h1>
+  <div>
+    <sui-input placeholder="Search..." icon="search" v-model="searchTerm"/>
+    <sui-card-group :items-per-row="3">
+      <sui-card v-for="project in projectsFiltered" :key="project._id">
+        <sui-card-content>
+          <sui-card-header>
+            {{project.name}}
+          </sui-card-header>
+          <sui-card-meta>{{project.pay}}</sui-card-meta>
+          <sui-card-meta>{{project.budget}}</sui-card-meta>
+          <sui-card-description>{{project.description}}</sui-card-description>
+        </sui-card-content>
+      </sui-card>
+    </sui-card-group>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'search',
+  data() {
+    return {
+      projects: [],
+      searchTerm: '',
+    };
+  },
+  created() {
+    this.fetch();
+  },
+  computed: {
+    projectsFiltered() {
+      return this.projects.filter(this.searchingFor(this.searchTerm));
+    },
+  },
+  methods: {
+    fetch() {
+      this.axios.get('/api/projects/all')
+        .then((res) => {
+          this.projects = res.data.projects;
+        });
+    },
+    searchingFor(term) {
+      let termL = term.toLowerCase();
+      return x =>
+        x.name.toLowerCase().includes(termL) ||
+        x.description.toLowerCase().includes(termL) || !term;
+    },
+  },
 };
 </script>
